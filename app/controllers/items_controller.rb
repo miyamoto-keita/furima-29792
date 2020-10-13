4250b41f5ee48127572e48c001f0578cdbe59191
miyamoto-keita <70692500+miyamoto-keita@users.noreply.github.com>
2020-10-13 20:29:57 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.order(id: :DESC)
   end
@@ -28,6 +28,11 @@ class ItemsController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+       @item.destroy
+       redirect_to action: :index
   end
 
   private
